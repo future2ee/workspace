47531973f1c244dc2e9e0fbc2145629767af47ed
mirae <mslcc458@naver.com>
2023-07-20 16:48:58 +0900

    0720

--- 5_Server/community/src/main/java/edu/kh/community/board/controller/BoardDetailServlet.java
+++ 5_Server/community/src/main/java/edu/kh/community/board/controller/BoardDetailServlet.java
@@ -1,6 +1,7 @@
 package edu.kh.community.board.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.kh.community.board.model.service.BoardService;
+import edu.kh.community.board.model.service.ReplyService;
 import edu.kh.community.board.model.vo.BoardDetail;
+import edu.kh.community.board.model.vo.Reply;
 
 @WebServlet("/board/detail")
 public class BoardDetailServlet extends HttpServlet{
@@ -26,6 +29,16 @@ public class BoardDetailServlet extends HttpServlet{
 			
 			// 게시글 정보 + 이미지리스트 조회
 			BoardDetail detail = service.selectBoardDetail(boardNo);
+			
+			// 게시글 상세조회된 내용이 있을 경우 댓글 목록 조회
+			if(detail != null) {
+				ReplyService rService = new ReplyService();
+				
+				List<Reply> rList = rService.selectReplyList(boardNo);
+				
+				req.setAttribute("rList", rList);
+				
+			}
 			
 			req.setAttribute("detail", detail);
 			
---
+++ 5_Server/community/src/main/webapp/resources/js/board/board.js
@@ -0,0 +1,43 @@
+// 상세조회 - 목록으로 버튼
+
+(function(){
+    const goToListBtn = document.getElementById("goToListBtn")
+
+    if(goToListBtn != null){ // 목록으로 버튼이 화면에 있을 때만 이벤트 추가
+
+        goToListBtn.addEventListener("click",function(){
+
+            // location 객체(BOM)
+
+            // 문자열.substring(시작, 끝) : 시작 이상 끝 미만 인덱스 까지 문자열 자르기
+
+            // 문자열.indexOf("검색 문자열", 시작인덱스)
+            // : 문자열에서 "검색 문자열"의 위치 (인데스)를 찾아서 반환
+            //   단. 시작 인덱스 이후 부터 검색
+
+            const pathname = location.pathname; //주소상에서 요청 경로 반환
+            //  /community/board/detail
+
+            // 이동할 주소 저장
+            let url = pathname.substring(0, pathname.indexOf("/",1));
+            //  /commnity
+
+            url += "/board/list?" //community/board/list?
+
+            // URL 내장 객체 : 주소 관려 ㄴ정보를 나타내는 객체
+            // location.href : 현재 페이지 주소 : 쿼리스트링
+            // URL.searchParams : 쿼리 스트링만 별도 객체로 반환
+
+            const params = new URL(location.href).searchParams;
+
+            const type = "type=" + params.get("type"); //type=1
+            const cp = "cp=" + params.get("cp"); // cp=1
+            // 조립
+            // /community/board/list?type=1&cp=1
+            url += type + "&" + cp;
+
+            // location.href ="주소"; -> 해당 주소로 이동
+            location.href = url;
+        });
+    }
+})();
