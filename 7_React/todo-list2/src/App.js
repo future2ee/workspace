e78e427c918cf7952a4309b1c5fc983f5df029e9
이미래 <mslcc458@naver.com>
2023-09-11 13:56:39 +0900

    0911

---
+++ 7_React/todo-list2/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, createContext} from 'react';
+import './App.css';
+import SignupContainer from './signup';
+
+function App() {
+
+  const [signupView, setSignupView] = useState(false);
+  return (
+    <>
+      <button onClick={()=>{setSignupView(!signupView)}}>
+        {signupView ? ('회원 가입 닫기') : ('회원 가입 열기')}
+      </button>
+
+      <div className='signup-wrapper'>
+        {signupView === true && (<SignupContainer/>)}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 7_React/todolistProject/src/main/java/edu/kh/todo/model/service/TodoService.java
@@ -0,0 +1,10 @@
+package edu.kh.todo.model.service;
+
+import edu.kh.todo.model.dto.TodoMember;
+
+public interface TodoService {
+
+	
+	int signup(TodoMember member);
+
+}
